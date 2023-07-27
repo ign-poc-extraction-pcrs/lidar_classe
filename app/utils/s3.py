5b32f8bf92fe0aa137b6ccfcfe6a2ae17f23758d
mmohadIGN <119047737+mmohadIGN@users.noreply.github.com>
2023-07-27 14:36:38 +0200

    Update s3.py

--- app/utils/s3.py
+++ app/utils/s3.py
@@ -32,7 +32,7 @@ class BucketAdpater:
         )
         
         self.bucket_name = os.environ.get('BUCKET_NAME')
-        logging.debug(type(self.bucket_name))
+        print(f"le type est :{type(self.bucket_name)")
         self.link_download = "https://storage.sbg.cloud.ovh.net/v1/AUTH_63234f509d6048bca3c9fd7928720ca1/ppk-lidar"
         
     def read_file(self, name_file) -> None:
