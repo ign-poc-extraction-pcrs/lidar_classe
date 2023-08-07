6b49ef265170e1b4b88f13964050eeb8834e2f7e
LArdilly <louis.ardilly@ign.fr>
2023-08-07 14:59:27 +0200

    add verification si le bloc est en base

--- app/utils/migration.py
+++ app/utils/migration.py
@@ -76,10 +76,11 @@ class Migration:
         print("insertion..")
         for bl in tqdm(BLOCS):
             if bl in dalles_s3["paquet_within_bloc"] and dalles_s3["paquet_within_bloc"][bl]:
-                for dalle in dalles_s3["paquet_within_bloc"][bl]:
-                    wkt = "POLYGON(({0} {1}, {2} {1}, {2} {3}, {0} {3}, {0} {1}))".format(dalle["bbox"][0], dalle["bbox"][1], dalle["bbox"][2], dalle["bbox"][3])
-                    dalles.append({"name": dalle["name"], "geom": wkt, "bloc": bl})
-
+                bloc_id = self.isBdd(bl)
+                if bloc_id is not None:
+                    for dalle in dalles_s3["paquet_within_bloc"][bl]:
+                        wkt = "POLYGON(({0} {1}, {2} {1}, {2} {3}, {0} {3}, {0} {1}))".format(dalle["bbox"][0], dalle["bbox"][1], dalle["bbox"][2], dalle["bbox"][3])
+                        dalles.append({"name": dalle["name"], "geom": wkt, "bloc_id": bloc_id["id"]})
         return dalles
     
     def export_bloc_extent(self):
@@ -93,13 +94,16 @@ class Migration:
         blocs = []
         # pour chaque bloc
         for bloc, dalles in index_json_files['paquet_within_bloc'].items():
-            polygon = unary_union([box(*dalle['bbox']) for dalle in dalles])
-            new_polygon = Polygon(polygon.exterior.coords).normalize()
-            multi_polygon = MultiPolygon([new_polygon])
-            geom_binary = dumps(multi_polygon)
-            
+            if dalles :
+                polygon = unary_union([box(*dalle['bbox']) for dalle in dalles])
+                if type(polygon) == Polygon:
+                    polygon = MultiPolygon([polygon])
+                # pour chaque polygon (du multi), on ne va récupérer que l'exterior ring et normaliser
+                multi_polygon = MultiPolygon([Polygon(polygon.exterior.coords).normalize() for polygon in polygon.geoms])
+                geom_binary = dumps(multi_polygon)
+                
 
-            blocs.append({"name": bloc, "geom": geom_binary})
+                blocs.append({"name": bloc, "geom": geom_binary})
         
         return blocs
 
@@ -117,6 +121,21 @@ class Migration:
                 # En cas d'erreur, annuler les changements et afficher l'erreur
                 self.connection.rollback()
                 print("Erreur lors de la suppression de la table :", e)
+    
+    def isBdd(self, bloc):
+        """verifie si un bloc est en bdd
+
+        Args:
+            bloc (str): nom bloc
+
+        Returns:
+            int, None: retourne l'id du bloc si il y'en a un
+        """
+        self.cursor.execute(f"SELECT id FROM bloc WHERE name = '{bloc}'")
+        bloc_id = self.cursor.fetchone()
+        if bloc_id :
+            return bloc_id
+        return None
 
 
 if __name__ == "__main__":
@@ -139,5 +158,5 @@ if __name__ == "__main__":
             FOREIGN KEY (bloc_id) REFERENCES bloc(id));
         """)
     migration.insert("bloc", "(name, geom)", "name", migration.export_bloc_extent(), f"(%s, %s)")
-    migration.insert("dalle", "(name, geom, bloc)", "name", migration.get_dalle_json(), f"(%s, %s, %s)")
+    migration.insert("dalle", "(name, geom, bloc_id)", "name", migration.get_dalle_json(), f"(%s, %s, %s)")
     migration.close_connection()
